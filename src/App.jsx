8959557ca52b9202dc7be8af13eef4aca6e4c61e
Maas <k.w.h.maas@tue.nl>
2024-08-28 15:21:45 +0200

    main page v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Publication from './Publication';
+import { FaGithub } from "react-icons/fa";
+import { FaGoogleScholar } from "react-icons/fa6";
+import { MdMail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+
+import me from './assets/me.jpg';
+
+
+const authorList = [
+  {
+    name: 'Kirsten Maas',
+    link: null,
+    is_me: true,
+  },
+  {
+    name: 'Nicola Pezzotti',
+    link: 'https://nicola17.github.io/',
+    is_me: false,
+  },
+  {
+    name: 'Amy Vermeer',
+    link: 'https://research.tue.nl/nl/persons/amy-je-vermeer',
+    is_me: false,
+  },
+  {
+    name: 'Danny Ruijters',
+    link: 'https://www.dannyruijters.nl/',
+    is_me: false,
+  },
+  {
+    name: 'Anna Vilanova',
+    link: 'https://www.tue.nl/en/research/researchers/anna-vilanova/',
+    is_me: false,
+  },
+]
+
+const publications = [
+  // {
+  //   title: 'NeRF-CA: Dynamic reconstruction of X-ray Coronary Angiography with extremely sparse-views',
+  //   authors: [authorList[0], authorList[3], authorList[4], authorList[1]],
+  //   venue: 'arXiv.org',
+  //   year: '2024',
+  //   paperLink: null,
+  //   demoLink: null,
+  //   projectLink: 'https://kirstenmaas.github.io/nerfca',
+  //   codeLink: 'https://github.com/kirstenmaas/NeRF-CA',
+  //   imageUrl: './nerf-ca.png',
+  // },
+  {
+    title: 'NeRF For 3D reconstruction From X-ray Angiography: Possibilities and Limitations',
+    authors: [authorList[0], authorList[1], authorList[2], authorList[3], authorList[4]],
+    venue: 'VCBM: Eurographics Workshop on Visual Computing for Biology and Medicine',
+    year: '2023',
+    paperLink: 'https://diglib.eg.org/items/1d80a1ce-ab59-4718-afd0-bc54e053e7f6',
+    demoLink: 'https://nerfforangiography.netlify.app/',
+    projectLink: null,
+    codeLink: null,
+    imageUrl: './nerf3d.png',
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Kirsten Maas</h1>
+        <div style={{ display: 'flex', gap: '5vw', justifyContent: 'center', flexWrap: 'wrap' }}>
+          <div style={{ textAlign: 'left', maxWidth: '50em', minWidth: '20em' }}>
+            Welcome! My name is Kirsten Maas. 
+            I am a 3rd year PhD student in Computer Science at the <a href='https://vis.win.tue.nl/'>Visualization cluster</a> at <a className='authorlink' href='https://research.tue.nl/en/persons/kirsten-wh-maas'>Eindhoven University of Technology</a> advised by <a href={authorList[3].link} className='authorlink'>Prof. Danny Ruijters</a>, <a className='authorlink' href={authorList[1].link}>Dr. Nicola Pezzotti</a>, and <a className='authorlink' href={authorList[4].link}>Prof. Anna Vilanova</a>. 
+            The topic of my PhD is AI-driven 3D reconstruction within the context of X-ray coronary angiography. 
+            This research is part of the <a className='authorlink' href='https://www.tue.nl/en/research/research-groups/eaisi/eaisi-business-operations/eindhoven-medtech-innovation-center/emtic-ai-lab'>e/MTIC collaboration</a> between Philips Healthcare, Catharina Hospital Eindhoven, and Eindhoven University of Technology.
+            
+          </div>
+          <img style={{ width: 'auto', height: 'auto', maxWidth: '10em', maxHeight: '10em', padding: '0.5em', border: '1px solid #dee2e6', borderRadius: '2%' }} src={me} alt='Picture of me' />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', marginTop: '1em', justifyContent: 'center', alignItems: 'center', gap: '1em' }}>
+        <a className='nocolor' href='https://github.com/kirstenmaas'><FaGithub size='1.5em' /></a>
+        <a className='nocolor' href='https://scholar.google.nl/citations?user=GG2-HcoAAAAJ&hl=en'><FaGoogleScholar size='1.5em' /></a>
+        <a className='nocolor' href='https://www.linkedin.com/in/kirsten-maas/'><FaLinkedin size='1.5em'/></a>
+        <a className='nocolor' href='mailto:k.w.h.maas@tue.nl'><MdMail size='1.7em' /></a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div style={{ textAlign: 'left', marginTop: '5vh' }}>
+        <h2 style={{ borderBottom: '1px solid #dee2e6'}}>Publications</h2>
+        {publications.map((publication, i) => (
+          <Publication
+            key={i}
+            title={publication.title}
+            authors={publication.authors}
+            venue={publication.venue}
+            year={publication.year}
+            paperLink={publication.paperLink}
+            demoLink={publication.demoLink}
+            projectLink={publication.projectLink}
+            codeLink={publication.codeLink}
+            imageUrl={publication.imageUrl}
+          />
+        ))}
+        
+      </div>
+    </div>
   )
 }
 
---
+++ src/Publication.jsx
@@ -0,0 +1,37 @@
+import { FaGlobeEurope } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFile } from "react-icons/fa";
+import { TbDeviceDesktopAnalytics } from "react-icons/tb";
+
+
+export default function Publication(props) {
+    return (
+        <div className='publication'>
+            <img style={{ maxWidth: '20em', maxHeight: '20em', padding: '0.5em', objectFit: 'cover', border: '1px solid #dee2e6', borderRadius: '2%' }} src={props.imageUrl} alt={props.altText} />
+            <div style={{ height: '100%', margin: 'auto 0' }}>
+                <h4>{props.title}</h4>
+                {props.authors.map((author, i) => {
+                    return (
+                        <span key={i}>
+                            <a 
+                                className={`authorlink ${author.is_me ? 'isme' : ''}`} 
+                                href={author.link}
+
+                            >
+                                {author.name}
+                            </a>
+                            {i < props.authors.length - 1 && ', '}
+                        </span>
+                    )
+                })}
+                <div>{props.venue}, {props.year}</div>
+                <div style={{ display: 'flex', gap: '1em', marginTop: '1em', alignItemsss: 'center'}}>
+                    {props.paperLink && <a className='nocolor' href={props.paperLink}><FaFile size='1.5em'/> Paper</a>}
+                    {props.projectLink && <a className='nocolor' href={props.projectLink}><FaGlobeEurope size='1.5em'/> Project page </a>}
+                    {props.codeLink && <a className='nocolor' href={props.codeLink}><FaGithub size='1.5em'/> Code</a>}
+                    {props.demoLink && <a className='nocolor' href={props.demoLink}><TbDeviceDesktopAnalytics size='1.7em'/>Demo</a>}
+                </div>
+            </div>
+        </div>
+    )
+}
